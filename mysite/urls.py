7b8ac5182669936e597027cfdeca6558aaef2d00
Leonardo Antonio Vanzella <leovanzella@gmail.com>
2018-05-31 17:32:52 -0300

    Create a route for /generate/ endpoint

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import GenerateRandomUserView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^generate/', GenerateRandomUserView.as_view(), name='generate'),
 ]
