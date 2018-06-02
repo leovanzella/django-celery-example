9cc855f4c660e0d2e67e8cd2344bd644108eeff8
Leonardo Antonio Vanzella <leovanzella@gmail.com>
2018-06-02 08:07:22 -0300

    Added celery app my_site

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,7 +1,16 @@
-import os
-from celery import Celery
+from __future__ import absolute_import, unicode_literals
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+import os
+import celery
+
+if os.environ.get('DJANGO_SETTINGS_MODULE') is None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        pass
+
 
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class GenerateRandomUserForm(forms.Form):
     total = forms.IntegerField(
         validators=[
-            MinValueValidator(50),
+            MinValueValidator(5),
             MaxValueValidator(500)
         ]
     )
