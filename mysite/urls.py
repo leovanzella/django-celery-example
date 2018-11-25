b758deccf5b28c9f5321861b300faef0dd4e95b7
Leonardo Antonio Vanzella <leovanzella@gmail.com>
2018-11-25 15:25:13 -0200

    Fix app ref

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from core.views import GenerateRandomUserView, UsersListView
+from .core.views import GenerateRandomUserView, UsersListView
 
 
 urlpatterns = [
